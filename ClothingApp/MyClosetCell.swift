d97524479c07f056d5d803e5912d27f2499e71a3
elee <elee@elees-imac.attlocal.net>
2015-11-25 16:18:32 -0800

    first commit

---
+++ ClothingApp/MyClosetCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyClosetCell.swift
+//  ClothingApp
+//
+//  Created by elee on 11/25/15.
+//  Copyright (c) 2015 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class MyClosetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
